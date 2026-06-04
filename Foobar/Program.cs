6b82e64f531d75e6ecfe92a18fde95c09c872d86
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-06-04 14:45:33 +0700

    add project Foobar

---
+++ Foobar/Program.cs
@@ -0,0 +1,24 @@
+﻿static void Foobar(int n)
+{
+    for(int x = 1; x <= n; x++)
+    {
+        if (x % 3 == 0 && x % 5 == 0)
+        {
+            Console.WriteLine("Foobar");
+        }
+        else if (x % 3 == 0)
+        {
+            Console.WriteLine("Foo");
+        }
+        else if (x % 5 == 0)
+        {
+            Console.WriteLine("Bar");
+        }
+        else
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
+
+Foobar(20); 

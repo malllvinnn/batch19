da607e7fed618a8ef0589caf7e9b86fc4b41d34c
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-06-08 14:55:10 +0700

    Learn Delegate(10%)

---
+++ Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Delegates
+{
+    class Program
+    {
+        delegate int Transformer(int X);
+        public static void Main(string[] args)
+        {
+            // DELEGATES BASIC
+            int Square(int x)
+            {
+                return x * x;
+            }
+
+            int Cube(int x)
+            {
+                return x * x * x;
+            }
+
+            Transformer t = Square;
+
+            int result1 = t(3);
+            Console.WriteLine(result1);
+            // Output: 9
+
+            t = Cube;
+            int result2 = t(3);
+            Console.WriteLine(result2);
+            // Output: 27
+        }
+    }
+}
